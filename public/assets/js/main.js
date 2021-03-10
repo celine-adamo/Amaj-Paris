82df560723bc59b7a2e081ee2e7e15e14fd0035a
Kilian MINETTO <killian.minetto@gmail.com>
2021-03-10 11:40:33 +0100

    décalage + sidebar sticky

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -2,16 +2,22 @@
 const menuBtn = document.querySelector('.menu-btn');
 const menuBtnBurger = document.querySelector('.menu-btn__burger');
 const sidebar = document.querySelector('.sidebar');
+const main = document.getElementById('main');
+const body = document.querySelector('body');
 
 let menuOpen = false;
 menuBtn.addEventListener('click', () => {
     if(!menuOpen) {
         menuBtn.classList.add('open');
         sidebar.classList.add('open');
+        main.classList.add('open');
+        body.classList.add('open');
         menuOpen = true;
     } else {
         menuBtn.classList.remove('open');
         sidebar.classList.remove('open');
+        main.classList.remove('open');
+        body.classList.remove('open');
         menuOpen = false;
     }
 });
@@ -32,14 +38,4 @@ function stickyNavbar() {
         navbar.classList.remove("sticky");
         sidebar.classList.remove("sticky");
     }
-
-    // if (navbar.classList.contains('sticky')) {
-    //     sidebar.classList.remove("open");
-    //
-    //     if (!menuOpen) {
-    //         sidebar.classList.add('sticky');
-    //     } else {
-    //         sidebar.classList.remove('sticky')
-    //     }
-    // }
 }
