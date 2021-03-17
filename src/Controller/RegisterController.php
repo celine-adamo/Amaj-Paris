a6bb30a433c0ba29208e7e3a4a2717f052b2aa2e
Kilian MINETTO <killian.minetto@gmail.com>
2021-03-17 11:05:59 +0100

    Connect form

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -41,6 +41,8 @@ class RegisterController extends AbstractController
             $this -> entityManager -> persist($user);
             $this -> entityManager -> flush();
 
+            return $this->redirectToRoute('home_page');
+
         }
 
         return $this->render('register/index.html.twig', [
