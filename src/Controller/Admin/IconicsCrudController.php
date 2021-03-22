e9d90b2958d466f0a27df38c5b948e7e6712440d
Kilian MINETTO <killian.minetto@gmail.com>
2021-03-22 08:51:18 +0100

    Way to add a pics for iconics in admin dashboard

--- src/Controller/Admin/IconicsCrudController.php
+++ src/Controller/Admin/IconicsCrudController.php
@@ -4,6 +4,9 @@ namespace App\Controller\Admin;
 
 use App\Entity\Iconics;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\SlugField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class IconicsCrudController extends AbstractCrudController
 {
@@ -12,14 +15,19 @@ class IconicsCrudController extends AbstractCrudController
         return Iconics::class;
     }
 
-    /*
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('name'),
+            TextField::new('img'),
+            ImageField::new('pics')
+                ->setBasePath('assets/images/iconics/')
+                ->setUploadDir('public/assets/images/iconics/')
+                ->setUploadedFileNamePattern('[randomhash].[extension]')
+                ->setRequired(false),
+            SlugField::new('slug')->setTargetFieldName('name'),
         ];
     }
-    */
+
 }
