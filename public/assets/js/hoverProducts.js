4c9ce1ec11b70fdbd8a48977a874a3597976a4e4
Kilian MINETTO <killian.minetto@gmail.com>
2021-04-07 10:48:12 +0200

    Refacto DressingController + hoverProduct.js

--- public/assets/js/hoverProducts.js
+++ public/assets/js/hoverProducts.js
@@ -1,26 +1,20 @@
 const baseUrl = window.location.href;
+let displacementImageRoute;
 
 if (baseUrl === 'http://127.0.0.1/dressing/creator') {
-    Array.from(document.querySelectorAll('.imgBox')).forEach((e) => {
-        const imgs = Array.from(e.querySelectorAll('img'));
-        new hoverEffect({
-            parent: e,
-            intensity: 0.3,
-            image1: imgs[0].getAttribute('src'),
-            image2: imgs[1].getAttribute('src'),
-            displacementImage: '../assets/images/displacements/10.jpg'
-        });
-    });
+    displacementImageRoute = '../assets/images/displacements/10.jpg';
 } else {
-    Array.from(document.querySelectorAll('.imgBox')).forEach((e) => {
-        const imgs = Array.from(e.querySelectorAll('img'));
-        new hoverEffect({
-            parent: e,
-            intensity: 0.3,
-            image1: imgs[0].getAttribute('src'),
-            image2: imgs[1].getAttribute('src'),
-            displacementImage: '../../assets/images/displacements/10.jpg'
-        });
-    });
+    displacementImageRoute = '../../assets/images/displacements/10.jpg';
 }
+
+Array.from(document.querySelectorAll('.imgBox')).forEach((e) => {
+    const imgs = Array.from(e.querySelectorAll('img'));
+    new hoverEffect({
+        parent: e,
+        intensity: 0.3,
+        image1: imgs[0].getAttribute('src'),
+        image2: imgs[1].getAttribute('src'),
+        displacementImage: displacementImageRoute
+    });
+});
 
--- src/Controller/DressingController.php
+++ src/Controller/DressingController.php
@@ -2,141 +2,62 @@
 
 namespace App\Controller;
 
-use App\Entity\Blaster;
-use App\Entity\Changer;
-use App\Entity\Creator;
-use App\Entity\Iconics;
-use App\Entity\Sider;
-use Doctrine\ORM\EntityManagerInterface;
+use App\Classes\ProductsIconics;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\Products;
 
 class DressingController extends AbstractController
-
 {
-    private EntityManagerInterface $entity;
-
-    public function __construct(EntityManagerInterface $entity)
-    {
-        $this->entity = $entity;
-    }
 
     /**
      * @Route("/dressing", name="dressing")
+     * @param ProductsIconics $products
+     * @return Response
      */
-    public function index(): Response
+    public function index(ProductsIconics $products): Response
     {
-        $products = $this->entity->getRepository(Products::class)->findAll();
-
         return $this->render('dressing/index.html.twig', [
-            'products' => $products,
+            'products' => $products->getAllProducts(),
         ]);
     }
 
     /**
      * @Route("/dressing/{slug}", name="one-dressing")
      * @param $slug
+     * @param ProductsIconics $product
      * @return Response
      */
-    public function one($slug): Response
+    public function one($slug, ProductsIconics $product): Response
     {
-        $product = $this->entity->getRepository(Products::class)->findOneBySlug($slug);
-        $iconics = $this->entity->getRepository(Iconics::class)->findAll();
+        $cloth = $product->getoneCloth($slug);
 
-        if (!$product) {
+        if (!$cloth['product']) {
             return $this->redirectToRoute('dressing');
         }
 
         return $this->render('dressing/one.html.twig', [
-            'product' => $product,
-            'iconics' => $iconics,
+            'product' => $cloth['product'],
+            'iconics' => $cloth['iconics'],
         ]);
     }
 
     /**
-     * @Route("/dressing/{slug}/{slugIconic}", name="img-on-clothes")
+     * @Route("/dressing/{slug}/{slugIconic}", name="product-iconic")
      * @param $slug
      * @param $slugIconic
+     * @param ProductsIconics $product
      * @return Response
      */
-    public function iconicOnClothes($slug, $slugIconic): Response {
-        $product = $this->entity->getRepository(Products::class)->findOneBySlug($slug);
-        $iconics = $this->entity->getRepository(Iconics::class)->findAll();
-        $iconic = $this->entity->getRepository(Iconics::class)->findOneBySlug($slugIconic);
-
-        if (!$product || !$iconic) {
-            return $this->redirectToRoute('dressing');
-        }
+    public function productIconic($slug, $slugIconic, ProductsIconics $product): Response
+    {
+        $cloth = $product->getProductIconics($slug, $slugIconic);
 
         return $this->render('dressing/one.html.twig', [
-            'product' => $product,
-            'iconics' => $iconics,
-            'iconic' => $iconic
+            'product' => $cloth['product'],
+            'iconic' => $cloth['iconic'],
+            'iconics' => $cloth['iconics'],
+            'clothes' => $cloth['clothes']
         ]);
-    }
-
-    /**
-     * @Route("/dressing/{slug}/{slugIconic}/{productIconic}", name="product-iconic")
-     * @param $slug
-     * @param $slugIconic
-     * @param $productIconic
-     * @return Response
-     */
-    public function productIconic($slug, $slugIconic, $productIconic): Response
-    {
-        $product = $this->entity->getRepository(Products::class)->findOneBySlug($slug);
-        $iconic = $this->entity->getRepository(Iconics::class)->findOneBySlug($slugIconic);
-        $iconics = $this->entity->getRepository(Iconics::class)->findAll();
-        $clothes = '';
-
-        switch ($product->getSlug()) {
-            case 'blaster':
-                $clothes = $this->getBlasterIconic($product->getSlug(), $iconic->getSlug());
-                break;
-            case 'changer':
-                $clothes = $this->getChangerIconic($product->getSlug(), $iconic->getSlug());
-                break;
-            case 'creator':
-                $clothes = $this->getCreatorIconic($product->getSlug(), $iconic->getSlug());
-                break;
-            case 'sider':
-                $clothes = $this->getSiderIconic($product->getSlug(), $iconic->getSlug());
-                break;
-            default:
-                break;
-        }
-
-        return $this->render('dressing/one.html.twig', [
-            'product' => $product,
-            'iconic' => $iconic,
-            'iconics' => $iconics,
-            'clothes' => $clothes
-        ]);
-    }
-
-    public function getBlasterIconic($product, $iconic): ?string
-    {
-        $slug = $product . '-' . $iconic;
-        return $this->entity->getRepository(Blaster::class)->findOneBySlug($slug);
-    }
-
-    public function getChangerIconic($product, $iconic)
-    {
-        $slug = $product . '-' . $iconic;
-        return $this->entity->getRepository(Changer::class)->findOneBySlug($slug);
-    }
-
-    public function getCreatorIconic($product, $iconic): ?string
-    {
-        $slug = $product . '-' . $iconic;
-        return $this->entity->getRepository(Creator::class)->findOneBySlug($slug);
-    }
-
-    public function getSiderIconic($product, $iconic): ?string
-    {
-        $slug = $product . '-' . $iconic;
-        return $this->entity->getRepository(Sider::class)->findOneBySlug($slug);
     }
 }
